04ebaefa859e404450e696c6c216e72e8cb1f91c
Jesse Martin <jmartin@yammer-inc.com>
2017-11-28 20:55:01 -0800

    read core temps from sensors

--- temps.cpp
+++ temps.cpp
@@ -3,12 +3,18 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <regex>
+#include <string>
 
 using namespace std;
 
+vector<double> cpu;
+vector<double> gpu;
+vector<double> drive;
+
 void render(vector<double> cpu, vector<double> gpu, vector<double> drive)
 {
-  printf("   CPU   | GPU | Drive\n");
+  cout << "   CPU   | GPU | Drive\n";
   for (int i = 10; i > 0; i--)
   {
     string row = " ";
@@ -27,11 +33,10 @@ void render(vector<double> cpu, vector<double> gpu, vector<double> drive)
   }
 }
 
-int main()
-{
+string probe_cpu() {
   char buffer[128];
   string result = "";
-  FILE *pipe = popen("ps ax", "r");
+  FILE *pipe = popen("sensors", "r");
   if (!pipe)
     printf("popen() failed!");
   try
@@ -48,9 +53,49 @@ int main()
     throw;
   }
   pclose(pipe);
-  vector<double> cpu = {45.2, 36.1, 93.0, 100.0};
-  vector<double> gpu = {29.0};
-  vector<double> drive = {26.2};
+  return result;
+}
+
+vector<string> split(const string &s, char delim) {
+  stringstream ss(s);
+  string item;
+  vector<string> tokens;
+  while (getline(ss, item, delim)) {
+    tokens.push_back(item);
+  }
+  return tokens;
+}
+
+vector<double> parse_cpu_temp(string cpu_temp) {
+  vector<string> lines = split(cpu_temp.c_str(), '\n');
+  vector<double> temps;
+  regex core_temp ("Core[^+|-]*[+|-]([^°]*).*");
+  smatch matches;
+  for (int i = 0; i < lines.size(); i++) {
+    regex_match(lines[i], matches, core_temp);
+    if (matches.size() == 2) {
+      double temp = stod(matches.str(1));
+      temps.push_back(temp);
+    }
+  }
+  return temps;
+}
+
+vector<double> get_cpu_temp() {
+  string cpu_temp = probe_cpu();
+  return parse_cpu_temp(cpu_temp);
+}
+
+void update_cpu_temp() {
+  cpu = get_cpu_temp();
+}
+
+int main()
+{
+  update_cpu_temp();
+
+  gpu = {29.0};
+  drive = {26.2};
   render(cpu, gpu, drive);
   return 0;
 }
