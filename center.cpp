c3882ce686529cf1aa75f1c2d7980e74e52468f9
Jesse Martin <jmartin@yammer-inc.com>
2017-11-28 21:24:50 -0800

    format output

---
+++ center.cpp
@@ -0,0 +1,37 @@
+// https://stackoverflow.com/questions/14861018/center-text-in-fixed-width-field-with-stream-manipulators-in-c/14861289#14861289
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+template<typename charT, typename traits = std::char_traits<charT> >
+class center_helper {
+  std::basic_string<charT, traits> str_;
+public:
+  center_helper(std::basic_string<charT, traits> str) : str_(str) {}
+  template<typename a, typename b>
+  friend std::basic_ostream<a, b>& operator<<(std::basic_ostream<a, b>& s, const center_helper<a, b>& c);
+};
+
+template<typename charT, typename traits = std::char_traits<charT> >
+center_helper<charT, traits> centered(std::basic_string<charT, traits> str) {
+  return center_helper<charT, traits>(str);
+}
+
+center_helper<std::string::value_type, std::string::traits_type> centered(const std::string& str) {
+  return center_helper<std::string::value_type, std::string::traits_type>(str);
+}
+
+template<typename charT, typename traits>
+std::basic_ostream<charT, traits>& operator<<(std::basic_ostream<charT, traits>& s, const center_helper<charT, traits>& c) {
+  std::streamsize w = s.width();
+  if (w > c.str_.length()) {
+    std::streamsize left = (w + c.str_.length()) / 2;
+    s.width(left);
+    s << c.str_;
+    s.width(w - left);
+    s << "";
+  } else {
+    s << c.str_;
+  }
+  return s;
+}
--- temps.cpp
+++ temps.cpp
@@ -1,12 +1,14 @@
-// #include "smc.c"
-// #include "smc.h"
 #include <stdio.h>
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <regex>
 #include <string>
 #include <chrono>
 #include <thread>
+#include <iomanip>
+#include <math.h>
+#include "center.cpp"
 
 using namespace std;
 
@@ -14,26 +16,43 @@ vector<double> cpu;
 vector<double> gpu;
 vector<double> drive;
 
+int col_width = 5;
+
 void render(vector<double> cpu, vector<double> gpu, vector<double> drive)
 {
-  cout << "\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K";
-  cout << "   CPU   | GPU | Drive\n";
+  ostringstream rendered;
+
+  string cpu_header = "CPU Temps";
+  int section_padding_right = 1;
+  int section_width = col_width * cpu.size() + section_padding_right;
+
+  rendered << "|" << setw(section_width) << centered(cpu_header) << "|\n";
   for (int i = 10; i > 0; i--)
   {
-    string row = " ";
+    rendered << "| ";
     for (int j = 0; j < cpu.size(); j++)
     {
       if (cpu[j] >= i * 10)
       {
-        row += "- ";
+        rendered << setw(col_width) << centered("==");
       }
       else
       {
-        row += "  ";
+        rendered << setw(col_width) << centered("..");
       }
     }
-    printf("%s|\n", row.c_str());
+    rendered << "|\n";
   }
+
+  rendered << "| ";
+  for (int i = 0; i < cpu.size(); i++) {
+    ostringstream temp;
+    temp << cpu.at(i);
+    rendered << setw(col_width) << centered(temp.str());
+  }
+  rendered << "|\n";
+
+  cout << rendered.str();
 }
 
 string probe_cpu() {
@@ -72,7 +91,7 @@ vector<string> split(const string &s, char delim) {
 vector<double> parse_cpu_temp(string cpu_temp) {
   vector<string> lines = split(cpu_temp.c_str(), '\n');
   vector<double> temps;
-  regex core_temp ("Core[^+|-]*[+|-]([^°]*).*");
+  regex core_temp ("Core[^+|-]*([^°]*).*");
   smatch matches;
   for (int i = 0; i < lines.size(); i++) {
     regex_match(lines[i], matches, core_temp);
@@ -101,6 +120,7 @@ int main()
     drive = {26.2};
     render(cpu, gpu, drive);
     this_thread::sleep_for(chrono::seconds(1));
+    cout << "\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K\033[A\033[2K";
   }
   return 0;
 }
